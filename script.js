dc535db0da0eae9667af5544bf7a892e4189cd5f
safiyahf <safiyah.sf@gmail.com>
2026-02-27 19:43:25 -0500

    fix: fix formatting

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function showChoice() {
     const radio = document.querySelector('input[name="choice"]:checked');
     const dropdown = document.getElementById('dropdown').value;
     let text = '';
-    if(radio) text += `Radio: ${radio.value} `;
+    if (radio) text += `Radio: ${radio.value} `;
     text += `Dropdown: ${dropdown}`;
     document.getElementById('choiceDisplay').innerText = text;
 }
@@ -31,7 +31,7 @@ function showChoice() {
 // Tab 6 ToDo List
 function addItem() {
     const input = document.getElementById('newItem');
-    if(input.value === '') return;
+    if (input.value === '') return;
     const li = document.createElement('li');
     li.innerText = input.value;
     li.onclick = () => li.classList.toggle('crossed');
@@ -50,17 +50,17 @@ function addItem() {
 
 // Tab 3 Pie Chart using Canvas
 const canvas = document.getElementById('pieChart');
-if(canvas) {
+if (canvas) {
     const ctx = canvas.getContext('2d');
-    const colors = ['red','blue','green','orange','purple'];
+    const colors = ['red', 'blue', 'green', 'orange', 'purple'];
     let start = 0;
     const slices = [30, 50, 20, 40, 60];
-    const total = slices.reduce((a,b) => a+b, 0);
+    const total = slices.reduce((a, b) => a + b, 0);
     slices.forEach((val, i) => {
-        const angle = (val/total) * 2 * Math.PI;
+        const angle = (val / total) * 2 * Math.PI;
         ctx.beginPath();
-        ctx.moveTo(150,100);
-        ctx.arc(150,100,100,start,start+angle);
+        ctx.moveTo(150, 100);
+        ctx.arc(150, 100, 100, start, start + angle);
         ctx.fillStyle = colors[i % colors.length];
         ctx.fill();
         start += angle;
